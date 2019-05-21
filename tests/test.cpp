01d9eca3a45d82861504c49336a12ac01d8c1ea8
KarileR <44968273+KarileR@users.noreply.github.com>
2019-05-21 12:17:49 +0300

    Add files via upload

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,1 +1,50 @@
+#include <gtest/gtest.h>
+#include "../headers/student.h"
+#include "../headers/person.h"
+#include "../headers/vector.h"
 
+
+TEST(vidurkis,check)
+{
+    student martynas;
+    martynas.set_mark(10);
+    martynas.set_mark(10);
+    martynas.set_egz(10);
+
+    martynas.Calculate_suVidurkiu();
+
+    EXPECT_EQ(martynas.getFinale_suVidurkiu(), 10);
+    
+}
+
+TEST(mediana,check)
+{
+    student elena;
+    elena.set_mark(8);
+    elena.set_mark(8);
+    elena.set_egz(8);
+
+    elena.Calculate_suMediana();
+  
+    EXPECT_EQ(elena.getFinale_suMediana(), 8);
+}
+
+Test(vector, check)
+{
+    Vector<student> student_list;
+    student martynas;
+    student elena;
+    student dominykas;
+    student_list.push_back(martynas);
+    student_list.push_back(elena);
+    student_list.push_back(dominykas);
+
+    EXPECT_EQ(student_list.size(),3);
+}
+
+
+int main(int argc, char **argv) 
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
